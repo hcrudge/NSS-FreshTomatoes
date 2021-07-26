226ff5b9a4d1aa78e510170fde5c56d91f6814a2
hcrudge <hcrudge1@gmail.com>
2021-07-26 09:22:46 -0500

    initial commit

---
+++ src/components/Popcorn.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "./Popcorn.css"
+import { ApplicationViews } from "./ApplicationViews"
+
+export const Popcorn = () => (
+    <>
+      
+        <ApplicationViews />
+    </>
+)

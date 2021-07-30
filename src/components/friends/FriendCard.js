3ecadd6b01bbab2b1c1262abcb00fa9ad7e45653
hcrudge <hcrudge1@gmail.com>
2021-07-29 21:53:30 -0500

    added display of friends and ability to add and delete friend records

---
+++ src/components/friends/FriendCard.js
@@ -0,0 +1,29 @@
+import React, {useContext} from "react"
+import { FriendContext } from "./FriendProvider"
+import { useHistory} from "react-router-dom"
+import "./Friend.css"
+
+export const FriendCard = ({friend}) => {
+
+const { deleteFriend } = useContext(FriendContext)
+const history = useHistory();
+
+const handleDeleteFriend = () => {
+    deleteFriend(friend.id)
+    .then(() =>{
+        history.push("/friends")
+    })
+}
+
+return (
+    <>
+     <section className="friend">
+        <h2 className="friend__name">{friend.friendName}</h2>
+        <button className ="delete__friend" onClick={() => {handleDeleteFriend()}}>Remove Friend</button>
+        </section>
+    </>
+)
+
+
+
+}
